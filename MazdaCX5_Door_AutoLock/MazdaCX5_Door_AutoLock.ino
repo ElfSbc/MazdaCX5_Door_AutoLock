0700b25947655b4179c4f68a2e5300cd3ed6d315
kandryunin <kandryunin@NOUT-017.people.local>
2016-11-30 01:40:11 +0400

    Рабочий вариант
    Если были открыты двери и скорость больше 20 кмч, то подается звуковой
    сигнал

--- MazdaCX5_Door_AutoLock/MazdaCX5_Door_AutoLock.ino
+++ MazdaCX5_Door_AutoLock/MazdaCX5_Door_AutoLock.ino
@@ -1,23 +1,26 @@
+// d
 #include <SPI.h>
 #include <mcp_can.h>
 
 #define SPI_CS_PIN			            10				// pin for CS CAN
-#define CAN_SPEED			              CAN_250KBPS		// can speed settings
+#define CAN_SPEED			              CAN_1000KBPS		// can speed settings
 #define CAN_FREQ			              MCP_16MHZ		// can frequency
 
 #define CAN_DOOR_ID			            0x43E			// can packet id for doors
-#define CAN_DOOR_BYTE			          5
-#define CAN_DOOR_FRONT_LEFT_BIT		  6
-#define CAN_DOOR_FRONT_RIGHT_BIT	  5
-#define CAN_DOOR_REAR_LEFT_BIT		  4
-#define CAN_DOOR_REAR_RIGHT_BIT		  3
+#define CAN_DOOR_BYTE			          4
+#define CAN_DOOR_FRONT_LEFT_BIT		  5
+#define CAN_DOOR_FRONT_RIGHT_BIT	  4
+#define CAN_DOOR_REAR_LEFT_BIT		  3
+#define CAN_DOOR_REAR_RIGHT_BIT		  2
 
-#define CAN_SPEED_ID			          0x202			// can packet id for Speed
-#define CAN_SPEED_BYTE			        5
-#define CAN_SPEED_BIT_FROM		      1
-#define CAN_SPEED_BIT_TO		        2
+#define CAN_SPEED_ID			          0x217			// can packet id for Speed
+#define CAN_SPEED_BYTE_1			       4
+#define CAN_SPEED_BYTE_2             5
 
-#define PIN_TO_LOCK                 13
+
+#define PIN_TO_LOCK                 8
+
+
 
 class Door{
 	private:
@@ -33,6 +36,7 @@ class Door{
 
 class Car{
 	private:		
+              char msgString2[150];
                 long unsigned int rxId;
                 unsigned char len = 0;
                 unsigned char rxBuf[8];
@@ -46,46 +50,57 @@ class Car{
 		Door doorRearRight 	;
 		
 		Car ();		
-		void canProcess(void);
+		void canProcess(MCP_CAN *CAN);
 
 };
 
 Car :: Car ()
 {
-	Serial.begin(115200);
+//  MCP_CAN *CAN = new MCP_CAN(SPI_CS_PIN);                        // Set CS pin
 
-        MCP_CAN *CAN = new MCP_CAN(SPI_CS_PIN);                        // Set CS pin
+//  if(CAN->begin(MCP_ANY, CAN_SPEED, CAN_FREQ) == CAN_OK) 
+//    Serial.print("CAN: [OK]");  
+//  else
+//    Serial.print("CAN: [ERROR]");  
 
-	if(CAN->begin(MCP_ANY, CAN_SPEED, CAN_FREQ) == CAN_OK) 
-		Serial.print("CAN: [OK]");  
-	else
-		Serial.print("CAN: [ERROR]");  
-	CAN->setMode(MCP_NORMAL);                     // Set operation mode to normal so the MCP2515 sends acks to received data.
-	
-	CAN->init_Filt(0, 0, CAN_DOOR_ID);
-	CAN->init_Filt(1, 0, CAN_SPEED_ID);
-	
+//    CAN->begin(MCP_ANY, CAN_SPEED, CAN_FREQ);
+//  CAN->setMode(MCP_NORMAL);                     // Set operation mode to normal so the MCP2515 sends acks to received data.
+  
+  //CAN->init_Filt(0, 0, CAN_DOOR_ID);
+  //CAN->init_Filt(1, 0, CAN_SPEED_ID);
 };
 
-void Car :: canProcess(void)
+void Car :: canProcess(MCP_CAN *CAN)
 		{
+
 			CAN->readMsgBuf(&rxId, &len, rxBuf);
 			if (rxId==CAN_DOOR_ID){	
 				doorFrontLeft. setState	( bitRead ( rxBuf[CAN_DOOR_BYTE], CAN_DOOR_FRONT_LEFT_BIT   ));
 				doorFrontRight.setState	( bitRead ( rxBuf[CAN_DOOR_BYTE], CAN_DOOR_FRONT_RIGHT_BIT  ));
 				doorRearLeft.  setState	( bitRead ( rxBuf[CAN_DOOR_BYTE], CAN_DOOR_REAR_LEFT_BIT    ));
 				doorRearRight. setState	( bitRead ( rxBuf[CAN_DOOR_BYTE], CAN_DOOR_REAR_RIGHT_BIT   ));
+        sprintf(msgString2, "- %d %d %d %d | %d", 
+              bitRead ( rxBuf[CAN_DOOR_BYTE], CAN_DOOR_FRONT_LEFT_BIT   ), 
+              bitRead ( rxBuf[CAN_DOOR_BYTE], CAN_DOOR_FRONT_RIGHT_BIT  ), 
+              bitRead ( rxBuf[CAN_DOOR_BYTE], CAN_DOOR_REAR_LEFT_BIT    ), 
+              bitRead ( rxBuf[CAN_DOOR_BYTE], CAN_DOOR_REAR_RIGHT_BIT   ),
+              rxBuf[3]);
+      //  Serial.println(msgString2);
+                
 			}
 			if (rxId==CAN_SPEED_ID){
-				speed = rxBuf[CAN_SPEED_BYTE]	>>	CAN_SPEED_BIT_TO	;
+				speed = rxBuf[CAN_SPEED_BYTE_1]*256+rxBuf[CAN_SPEED_BYTE_2];
+//               sprintf(msgString2, "- speed: %d", speed/100);
+//               Serial.println(msgString2);
 			}	
 		};
 
 /////////////////////////////////////////////////////
 Car mazda;
+
+MCP_CAN CAN0(SPI_CS_PIN);                          // Set CS to pin 10
   
 bool wasOpened = true;  // true if one of the doors was opened
-int lockPin = PIN_TO_LOCK;        // pin 5V to close doors
 
 char msgString[150];
 /////////////////////////////////////////////////////
@@ -93,33 +108,46 @@ char msgString[150];
 
 void setup()
 {
+  Serial.begin(115200);
+  Serial.println("=======================OK========================\n");
+  if(CAN0.begin(MCP_ANY, CAN_SPEED, CAN_FREQ) == CAN_OK) 
+    Serial.print("CAN: [OK]\n");  
+  else
+    Serial.print("CAN: [ERROR]\n");  
+  CAN0.setMode(MCP_NORMAL);                     // Set operation mode to normal so the MCP2515 sends acks to received data.
+  
+//  CAN0.init_Filt(0, 0, CAN_DOOR_ID);
 
-  pinMode(lockPin, OUTPUT);   
-  digitalWrite(lockPin, LOW);  
+  pinMode(PIN_TO_LOCK, OUTPUT);   
+  digitalWrite(PIN_TO_LOCK, LOW);
 }
 
 void loop()
 {
-  mazda.canProcess();
-
-  sprintf(msgString, "%d %d %d %d %d %d  0V\n", wasOpened, mazda.doorFrontLeft.getState(), mazda.doorFrontRight.getState(), mazda.doorRearLeft.getState(), mazda.doorRearRight.getState(), mazda.speed);
-  Serial.print(msgString);
-
+  mazda.canProcess(&CAN0);
 
   wasOpened = wasOpened || mazda.doorFrontLeft.getState() || mazda.doorFrontRight.getState() || mazda.doorRearLeft.getState() || mazda.doorRearRight.getState();
   
-  if (wasOpened & mazda.speed > 10){
-      wasOpened = false;
+  if (wasOpened & mazda.speed/100>20 &!mazda.doorFrontLeft.getState() & !mazda.doorFrontRight.getState() & !mazda.doorRearLeft.getState() & !mazda.doorRearRight.getState()){
+
+      sprintf(msgString, "+ %d %d %d %d | %d", mazda.doorFrontLeft.getState(), mazda.doorFrontRight.getState(), mazda.doorRearLeft.getState(), mazda.doorRearRight.getState(), mazda.speed/100);
+   Serial.println(msgString);
+   
+      tone(PIN_TO_LOCK, 1000,100);
+      delay(100);              // wait for a second
+      noTone(PIN_TO_LOCK);
+      wasOpened = false;      
+  //delay(100);              // wait for a second
+//      digitalWrite(PIN_TO_LOCK, HIGH);
+//      delay(1000);
+//      digitalWrite(PIN_TO_LOCK, LOW);      
+//      delay(1000);
       
-      digitalWrite(lockPin, HIGH);
-      delay(1000);
-      digitalWrite(lockPin, LOW);      
-      delay(1000);
-      
-      sprintf(msgString, "%d %d %d %d %d %d +5V\n", wasOpened, mazda.doorFrontLeft.getState(), mazda.doorFrontRight.getState(), mazda.doorRearLeft.getState(), mazda.doorRearRight.getState(), mazda.speed);
-      Serial.print(msgString);
+
 
   }
+
+  
 
 }
   
